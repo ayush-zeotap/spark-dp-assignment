57b98c859d29ad123515a21340d99057220c2a25
Ayush Mittal <ayushmittal@ZT-Mac-IN-Ayush-Mittal.local>
2021-08-18 00:06:18 +0530

    Added remaining datasets and created feature source columns

--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -10,12 +10,16 @@ object Driver {
 
     val schemaText = spark.sparkContext.wholeTextFiles("./src/main/resources/outputSchema.avsc").collect()(0)._2
     val inputPath_4 = "src/main/resources/part-00000-a4794630-9584-4152-b5ed-595fd7608322.avro"
-    val inputPath_212 = "src/main/resources/part-00000-64940e90-dcc3-4d23-9a38-c7e1e1d24820.avro"
+    val inputPath_212_1 = "src/main/resources/part-00000-64940e90-dcc3-4d23-9a38-c7e1e1d24820.avro"
     val inputPath_316 = "src/main/resources/part-00000-a3521293-47b8-4b2c-983a-2b973319fd51.avro"
+    val inputPath_212_2 = "src/main/resources/part-00000-ec4ac14a-4d9b-4675-b2a5-224784c22950.avro"
 
     val inputDf_4 = spark.read.option("avroSchema", schemaText).avro(inputPath_4)
-    val inputDf_212 = spark.read.option("avroSchema", schemaText).avro(inputPath_212)
+    val inputDf_212_1 = spark.read.option("avroSchema", schemaText).avro(inputPath_212_1)
     val inputDf_316 = spark.read.option("avroSchema", schemaText).avro(inputPath_316)
+    val inputDf_212_2 = spark.read.option("avroSchema", schemaText).avro(inputPath_212_2)
+
+    val inputDf_212 = inputDf_212_1.union(inputDf_212_2)
 
     val priority_matrix = spark.read.json("src/main/scala/priority.json")
 
@@ -31,28 +35,36 @@ object Driver {
       priority
     }
 
+    def features_dpid(data_feature: String) = {
+      val target_feature = priority_matrix.select(data_feature).head().getList(0).toArray
+      var priority = Seq[Column]()
+      for (dp <- target_feature) {
+        if (dp == 4) priority = priority :+ inputDf_4("DpId")
+        else if (dp == 212) priority = priority :+ inputDf_212("DpId")
+        else priority = priority :+ inputDf_316("DpId")
+      }
+      priority
+    }
+
     val merged_final = inputDf_4.join(inputDf_212, Seq("Identifier")).join(inputDf_316, Seq("Identifier"))
       .select(inputDf_4("Identifier"), coalesce(features_order("Age"): _*).as("Age"),
         coalesce(features_order("Gender"): _*).as("Gender"),
         coalesce(features_order("Zipcode"): _*).as("Zipcode"),
         coalesce(features_order("Device"): _*).as("Device"),
-        coalesce(features_order("Language"): _*).as("Language"))
+        coalesce(features_order("Language"): _*).as("Language"),
+        coalesce(features_dpid("Gender"): _*).as("Gender_dpid"),
+        coalesce(features_dpid("Age"): _*).as("Age_dpid"),
+        coalesce(features_dpid("Zipcode"): _*).as("Zipcode_dpid"),
+        coalesce(features_dpid("Device"): _*).as("Device_dpid"),
+        coalesce(features_dpid("Language"): _*).as("Language_dpid"))
+
     val overlap = inputDf_4.join(inputDf_316, "Identifier")
 
     val distinct_id = merged_final.count()
 
-    val dp4_age = merged_final.filter(merged_final("DpId") === 4 && merged_final("Age").isNotNull).count()
-    val dp212_age = merged_final.filter(merged_final("DpId") === 212 && merged_final("Age").isNotNull).count()
-    val dp316_age = merged_final.filter(merged_final("DpId") === 316 && merged_final("Age").isNotNull).count()
-
-    val dp4_gender = merged_final.filter(merged_final("DpId") === 4 && merged_final("Gender").isNotNull).count()
-    val dp212_gender = merged_final.filter(merged_final("DpId") === 212 && merged_final("Gender").isNotNull).count()
-    val dp316_gender = merged_final.filter(merged_final("DpId") === 316 && merged_final("Gender").isNotNull).count()
-
-    val dp4_zipcode = merged_final.filter(merged_final("DpId") === 4 && merged_final("Zipcode").isNotNull).count()
-    val dp212_zipcode = merged_final.filter(merged_final("DpId") === 212 && merged_final("Zipcode").isNotNull).count()
-    val dp316_zipcode = merged_final.filter(merged_final("DpId") === 316 && merged_final("Zipcode").isNotNull).count()
-
+    val dp_age = merged_final.groupBy("Age_dpid").count()
+    val dp_gender = merged_final.groupBy("Gender_dpid").count()
+    val dp_zipcode = merged_final.groupBy("Zipcode_dpid").count()
 
     def age_bucket = udf((age: Int) =>
       if (age <= 18) 18
